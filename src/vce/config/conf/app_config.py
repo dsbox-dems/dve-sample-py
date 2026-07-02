4d8d88c0141c17e86fa4dbc554d65066e1c1539c
GC.Pelosi <giovanni.pelosi@unimib.it>
2026-07-02 17:33:11 +0200

    WIP: Yaml config as local() wrapper

--- src/vce/config/conf/app_config.py
+++ src/vce/config/conf/app_config.py
@@ -1,10 +1,14 @@
+from __future__ import annotations
 import os.path
-from typing import Any, cast, override
+from typing import Any, cast, override, TYPE_CHECKING
 from piny import YamlLoader
 
 from vce.config.conf.base.base_config import BaseConfigImpl, BaseConfigStore
-from vce.config.conf.db import DbConfig
 from vce.config.conf import AppConfigConsts, AppConfigEx
+from vce.config.conf import ProjectConfig, get_local_config
+
+if TYPE_CHECKING:
+    from vce.config.conf.db import DbConfig
 
 
 class AbsAppConfig(
@@ -14,7 +18,7 @@ class AbsAppConfig(
     def __init__(self, name: str, conf: dict | None = None):
         super().__init__(name, conf)
 
-    def db(self, db_name: str) -> DbConfig:
+    def db(self, db_name: str) -> "DbConfig":
         # ruff: noqa: PLC0415
         from vce.config.conf.db.db_config import DbConfigFactory
 
@@ -29,10 +33,23 @@ class YamlAppConfig(AbsAppConfig):
         super().__init__(name, conf)
 
     @classmethod
-    def create(cls, name: str, config_path: str) -> AbsAppConfig:
+    def create(cls, name: str, config_path: str) -> "YamlAppConfig":
         try:
             raw_conf = YamlLoader(path=config_path).load()
             conf = cast("dict[str, Any]", raw_conf)
+            result = YamlAppConfig(name, conf)
+            return result
+        except Exception as ex:
+            print(type(ex))
+            print(ex.args)
+            print(ex)
+            raise ex
+
+    @classmethod
+    def create_empty(cls, name: str) -> "YamlAppConfig":
+        try:
+            loc: ProjectConfig = get_local_config()
+            conf: dict[str, Any] = loc.local
             result = YamlAppConfig(name, conf)
             return result
         except Exception as ex:
@@ -47,7 +64,7 @@ class AppConfigStore(BaseConfigStore[YamlAppConfig]):
         super().__init__(YamlAppConfig)
 
     @override
-    def load_config(self, what=AppConfigConsts.CONFIG_S_DEFAULT) -> AbsAppConfig:
+    def load_config(self, what=AppConfigConsts.CONFIG_S_DEFAULT) -> YamlAppConfig:
         config_path = self.config_path(what)
         if not os.path.exists(config_path):
             raise ValueError(f"Config Name {what} file not found: {config_path}")
